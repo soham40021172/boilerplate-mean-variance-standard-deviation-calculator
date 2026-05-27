503a2754e62ac0778217efdbadac553a67532759
soham40021172 <sohamg2711@gmail.com>
2026-05-27 18:53:29 +0000

    Completed

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,43 @@
 import numpy as np
 
-def calculate(list):
+def calculate(list_input):
 
+    if len(list_input) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    matrix = np.array(list_input).reshape(3, 3)
 
+    calculations = {
+        "mean": [
+            matrix.mean(axis=0).tolist(),  # Calculations along columns
+            matrix.mean(axis=1).tolist(),  # Calculations along rows
+            matrix.mean().item(),  # Calculation for the flattened matrix
+        ],
+        "variance": [
+            matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(),
+            matrix.var().item(),
+        ],
+        "standard deviation": [
+            matrix.std(axis=0).tolist(),
+            matrix.std(axis=1).tolist(),
+            matrix.std().item(),
+        ],
+        "max": [
+            matrix.max(axis=0).tolist(),
+            matrix.max(axis=1).tolist(),
+            matrix.max().item(),
+        ],
+        "min": [
+            matrix.min(axis=0).tolist(),
+            matrix.min(axis=1).tolist(),
+            matrix.min().item(),
+        ],
+        "sum": [
+            matrix.sum(axis=0).tolist(),
+            matrix.sum(axis=1).tolist(),
+            matrix.sum().item(),
+        ],
+    }
 
     return calculations
